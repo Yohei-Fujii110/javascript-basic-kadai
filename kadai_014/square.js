1a7eed5db0748fa794b840072555b4d5f9cdf9fb
Yohei-Fujii110 <155057244+Yohei-Fujii110@users.noreply.github.com>
2024-02-19 17:09:07 +0900

    Update square.js

--- kadai_014/square.js
+++ kadai_014/square.js
@@ -3,15 +3,8 @@ const square = (num) => {
   console.log(`${num}の2乗は${num ** 2}です`);
 }
 
-// 1～100の任意の整数を生成する
-function getRandomInt(min, max) {
-  min = Math.ceil(1);
-  max = Math.floor(100);
-  return Math.floor(Math.random() * 99 + 1);
-}
-
-// 生成された整数をrandomIntと定義する
-const randomInt = getRandomInt();
+// 1～20でランダムに生成された整数をrandomIntと定義する
+const randomInt = Math.floor(Math.random() * 19 + 1);
 
 // 関数を呼び出し、引数としてrandomIntを渡す
 square(randomInt);
